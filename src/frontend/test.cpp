5de2d4a2fe3e79e0eb89ad3cc56d65e0759735d0
adam6446654 <adamshahid20060000@gmail.com>
2026-07-16 22:45:40 +0500

    add backspace logic

--- src/frontend/test.cpp
+++ src/frontend/test.cpp
@@ -7,16 +7,35 @@ using namespace std;
 int main(){
 
     string command = "";
-
+    int comlength = 0;
 
     char c;
 
     while(true){
         c = _getch();
+        comlength++;
+        if(c == 127 || c == 8){
+            if(comlength > 1){
+                system("cls");
+                command[comlength - 2] = '\0';
+
+                if(comlength -2 < 0){
+                    cout << command;
+                }else{
+                    comlength = comlength - 2;
+                }
+                cout << command;
+            }else{
+                command = "";
+            }
+            continue;
+        }
         if(c == '\r'){ // \n for linux
             break;
         }
         command += c;
         cout << c;
     }
+
+    cout << "final command: " << command << endl;
 }
